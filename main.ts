79a74f76f55e92e8bb8efbdca6d3d470a653aafb
Sekikim <132802611+Sekikim@users.noreply.github.com>
2023-05-07 15:17:40 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -17,6 +17,13 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.pause(100)
     }
+    basic.clearScreen()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.Ghost)
+    basic.pause(100)
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 4; index++) {
@@ -37,7 +44,9 @@ input.onButtonPressed(Button.B, function () {
             `)
         basic.pause(100)
     }
+    basic.clearScreen()
 })
+basic.showString("LOVE")
 basic.forever(function () {
 	
 })

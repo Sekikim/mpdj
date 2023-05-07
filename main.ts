bb0223a1610e02db07abc796404d64cef806d46d
Sekikim <132802611+Sekikim@users.noreply.github.com>
2023-05-07 15:03:01 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,38 +1,42 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . # . .
-        . # . # .
-        . . . . .
-        `)
-    basic.pause(100)
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . # . .
+            . # . # .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . # # # .
-        # . . . #
-        # . . . #
-        # . . . #
-        . # # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . . . . .
-        . . # . .
-        . # . # .
-        . . # . .
-        . . . . .
-        `)
-    basic.pause(100)
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . # # # .
+            # . . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+        basic.pause(100)
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            . . # . .
+            . . . . .
+            `)
+        basic.pause(100)
+    }
 })
 basic.forever(function () {
 	
